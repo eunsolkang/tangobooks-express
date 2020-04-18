ea67c69c6ad6f131127b8ed27094b803afa6adc9
강은솔 <eunsol2953@gmail.com>
2020-04-18 21:38:38 +0900

    tngo last

--- src/config/express.ts
+++ src/config/express.ts
@@ -34,7 +34,7 @@ app.use(
     saveUninitialized: false,
     store : require('mongoose-session')(mongoose),
     cookie: {
-
+      domain : '.tngobooks.com',
       secure : false,
       maxAge: 86400 * 1000 // 24 hours in milliseconds
     }
